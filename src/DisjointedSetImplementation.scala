950cd3e1c036da34d5e162e3d5dd3e78bf5faef0
jadephung <jadephung.29@gmail.com>
2019-07-22 18:10:28 +0700

    disjointed Set finish coding not tested

--- src/DisjointedSetImplementation.scala
+++ src/DisjointedSetImplementation.scala
@@ -1,5 +1,6 @@
 class DisjointedSetImplementation(var tableList: Array[Int]) {
   var setByInput = makeSet(tableList)
+  var rankOfTree = Array.fill(setByInput.length)(0)
 
   /*difference here is node containing value too, not just table indexing */
   def makeSet(input: Array[Int]): Array[tableValue] = {
@@ -11,20 +12,37 @@ class DisjointedSetImplementation(var tableList: Array[Int]) {
   def find(tableNumber: Int): tableValue = {
     /*Since we do path compression here we have to implement 2 job:
     * 1 is return the tableValue
-    * 2 is modify the parent of each node it tranverse through */
-
-    null
-
+    * 2 is modify the parent of each node it tranverse through
+    * let's try to write this in tail recursive manner */
+    var rootID = setByInput(tableNumber).parentNode
+    if (rootID != tableNumber) {
+      while (rootID != setByInput(rootID).parentNode) {
+        rootID = setByInput(rootID).parentNode
+      }
+    }
+    setByInput.update(tableNumber, tableValue(rootID, setByInput(tableNumber).nodeValue))
+    setByInput(rootID)
   }
 
   def union(tableNumber1: Int, tableNumber2: Int): Unit = {
     /*Here we do rank heuristic union, which mean we don't have to return anything, just do
     * Check the rank and union the two table by rank  */
-    null
+    val rootTableNumber1 = find(tableNumber1)
+    val rootTableNumber2 = find(tableNumber2)
+    if (rootTableNumber1.parentNode == rootTableNumber2.parentNode) {
+      return null
+    } else if (rankOfTree(rootTableNumber1.parentNode) > rankOfTree(rootTableNumber2.parentNode)) {
+      setByInput.update(rootTableNumber2.parentNode, tableValue(rootTableNumber1.parentNode, rootTableNumber2.nodeValue))
+      if (rankOfTree(rootTableNumber1.parentNode) == rankOfTree(rootTableNumber2.parentNode)) {
+        rankOfTree.update(rootTableNumber1.parentNode, rankOfTree(rootTableNumber1.parentNode) + 1)
+      }
+
+    }
+
   }
 
 
 }
 
 
-case class tableValue(nodeNumber: Int, nodeValue: Int)
+case class tableValue(parentNode: Int, nodeValue: Int)
--- src/mergeTable.scala
+++ src/mergeTable.scala
@@ -1,3 +1,12 @@
 object mergeTable {
+def main(args: Array[String]): Unit ={
+  val input = Array(1,1,1,1,1,1,1,1)
+  val disjointedSet= new DisjointedSetImplementation(input)
+
+  println(disjointedSet.find(3))
+  disjointedSet.setByInput.foreach(x => println(x))
+
+}
+
 
 }
